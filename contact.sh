ab272f5382e04e019dca3f3c28e68b92f700deb5
soumymafia <kehed200p@gmail.com>
2020-12-15 02:05:50 +0700

    “scriptssh“

---
+++ contact.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Contact Me
+echo -e "   ======================"
+echo -e "   |   Contact Author   |"
+echo -e "   ----------------------"
+echo -e ""
+echo -e "   Telegram  : t.me/Soumy_R"
+echo -e "   WhatsApp  : +62 821 436 614 36"
+echo -e "   Instagram : ########"
